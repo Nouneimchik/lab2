4a42c010c664a7a1c3a01d3c8e13f5b7e774d059
Vadim Hutsol <wadymy@gmail.com>
2024-09-23 09:56:57 +0300

    Виконав завдання 3_1

--- JS/task-3.js
+++ JS/task-3.js
@@ -1,14 +1,12 @@
 function getElementWidth(content, padding, border) {
 
     const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding) * 2; // Падінг з обох сторін
-    const borderWidth = parseFloat(border) * 2; // Бордер з обох сторін
+    const paddingWidth = parseFloat(padding) * 2;
+    const borderWidth = parseFloat(border) * 2;
 
-    // Обчислюємо загальну ширину
     return contentWidth + paddingWidth + borderWidth;
 }
 
-// Перевірка роботи функції
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
